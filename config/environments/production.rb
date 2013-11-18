0af5712c1fc757a2905ee5d3b399f0cb5fdff06a
drs-smellie <makes.melly@gmail.com>
2013-11-18 16:56:37 +0100

    hard-coded mail hostname
    because devise is stupid.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,7 @@ Bloccit::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+  # needed for devise
+  config.action_mailer.default_url_options = { :host => 'drs-smellie-bloccit.herokuapp.com' }
 end
