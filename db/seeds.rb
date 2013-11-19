1724ce4417cf68f150104d2da36960d46db4c5dd
drs-smellie <makes.melly@gmail.com>
2013-11-19 17:07:29 +0100

    Associated and nested topics and posts.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,14 @@
 require 'faker'
 
+# Create 15 topics
+topics = []
+15.times do
+  topics << Topic.create(
+    name: Faker::Lorem.words(rand(1..10)).join(" "),
+    description: Faker::Lorem.paragraph(rand(1..4))
+  )
+end
+
 rand(4..10).times do
   password = Faker::Lorem.characters(10)
   u = User.new(
@@ -16,22 +25,50 @@ rand(4..10).times do
   # to avoid sending an email. The `save` method updates the database.
 
   rand(5..12).times do
+    topic = topics.first # getting the first topic here
     p = u.posts.create(
+      topic: topic,
       title: Faker::Lorem.words(rand(1..10)).join(" "), 
       body: Faker::Lorem.paragraphs(rand(1..4)).join("\n"))
     # set the created_at to a time within the past year
     p.update_attribute(:created_at, Time.now - rand(600..31536000))
 
-    rand(3..7).times do
+    topics.rotate! # add this line to move the first topic to the last, so that posts get assigned to different topics.
+
+
+  rand(3..7).times do
       p.comments.create(
         body: Faker::Lorem.paragraphs(rand(1..2)).join("\n"))
     end
   end
 end
 
-u = User.first
-u.skip_reconfirmation!
-u.update_attributes(name: 'Melanie', email: 'melanie.keatley@gmail.com', password: 'bloccit1', password_confirmation: 'bloccit1')
+u = User.new(
+  name: 'Admin User',
+  email: 'melanie.keatley@gmail.com', 
+  password: 'bloccit1', 
+  password_confirmation: 'bloccit1')
+u.skip_confirmation!
+u.save
+u.update_attribute(:role, 'admin')
+
+u = User.new(
+  name: 'Moderator User',
+  email: 'moderator@gmail.com', 
+  password: 'bloccit1', 
+  password_confirmation: 'bloccit1')
+u.skip_confirmation!
+u.save
+u.update_attribute(:role, 'moderator')
+
+u = User.new(
+  name: 'Member User',
+  email: 'member@gmail.com', 
+  password: 'bloccit1', 
+  password_confirmation: 'bloccit1')
+u.skip_confirmation!
+u.save
+
 
 puts "Seed finished"
 puts "#{User.count} users created"
