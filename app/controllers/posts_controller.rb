04f9cc78ff01a3bf229eed305978c3dcf180c5d8
drs-smellie <makes.melly@gmail.com>
2013-11-25 17:06:18 +0100

    Added comments

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,14 +1,16 @@
 class PostsController < ApplicationController
   def show
-    @post = Post.find(params[:id])
     @topic = Topic.find(params[:topic_id])
+    @post = Post.find(params[:id])
+    @comments = @post.comments
+    @comment = Comment.new
   end
 
   def new
     @topic = Topic.find(params[:topic_id])
     @post = Post.new
     authorize! :create, Post, message: "You need to be a member to create a new post."
-      end
+  end
 
   def create
     @topic = Topic.find(params[:topic_id])
