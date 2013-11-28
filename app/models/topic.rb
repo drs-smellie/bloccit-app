3c083b8a37d5c1428a6e58763d0dea1af8b13a2e
drs-smellie <makes.melly@gmail.com>
2013-11-28 15:40:55 +0100

    Scoped topics for privacy

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,4 +1,5 @@
 class Topic < ActiveRecord::Base
     has_many :posts, dependent: :destroy
     default_scope order('created_at DESC')
+    scope :visible_to, lambda { |user| user ? scoped : where(public: true) }
 end
