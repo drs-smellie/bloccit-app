7cba571068bf6c822320bc571bf1c21a22833478
drs-smellie <makes.melly@gmail.com>
2013-11-29 13:58:51 +0100

    Created user profile

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Bloccit::Application.routes.draw do
 
   devise_for :users
 
+  resources :users, only: [:show]
+
   resources :topics do
     resources :posts, except: [:index] do
       resources :comments, only: [:create, :destroy]
