ae0b3456e53d86506631f6ef13d368cedeb39c31
drs-smellie <makes.melly@gmail.com>
2013-11-29 16:54:29 +0100

    Added additional design updates.

---
+++ spec/vote_spec.rb
@@ -0,0 +1,10 @@
+require 'spec-helper'
+
+describe Vote do
+  describe "up_vote?" do
+    it "returns true for an up vote" do
+      v = Vote.new(value: 1)
+      v.up_vote?.should be (true)
+    end
+  end
+end
